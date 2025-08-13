119972447ea197d847cfa72e727e8216563fd89f
Prakash Nayak <prakash@thedevelopment.com.au>
2025-08-13 12:05:26 +0545

    local upload

---
+++ config/autoload/local.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Local application configuration
+ *
+ * Insert your local database credentials here
+ * and provide the email address the system should use.
+ */
+
+return [
+    'db' => [
+        'database' => 'tdasmkbs',
+        'username' => 'root',
+        'password' => '',
+
+        'hostname' => 'localhost',
+        'port' => null,
+    ],
+    'mail' => [
+        'type' => 'file', // or 'smtp' or 'smtp-tls' or type=> 'sendmail' (or 'file', to not send, but save to file (data/mails/))
+        'address' => 'smktennis@hotmail.com',
+        // Make sure 'bookings.example.com' matches the hosting domain when using type 'sendmail'
+
+        'host' => '?', // for 'smtp' type only, otherwise remove or leave as is
+        'user' => '?', // for 'smtp' type only, otherwise remove or leave as is
+        'pw' => '?', // for 'smtp' type only, otherwise remove or leave as is
+
+        'port' => 'auto', // for 'smtp' type only, otherwise remove or leave as is
+        'auth' => 'plain', // for 'smtp' type only, change this to 'login' if you have problems with SMTP authentication
+    ],
+    'i18n' => [
+        'choice' => [
+            'en-US' => 'English',
+            'de-DE' => 'Deutsch',
+
+            // More possible languages:
+            // 'fr-FR' => 'Français',
+            // 'hu-HU' => 'Magyar',
+        ],
+
+        'currency' => 'AUD',
+
+        // The language is usually detected from the user's web browser.
+        // If it cannot be detected automatically and there is no cookie from a manual language selection,
+        // the following locale will be used as the default "fallback":
+        'locale' => 'en-US',
+    ],
+];
